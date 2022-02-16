6ae0ab1bf03603abf3689e4cd556ec7ab6f71f9e
Mitchell Kennedy <mkennedy@alphatech-computing.com>
2022-02-16 12:52:15 -0500

    Added twitch app and started basic code layout.

--- pidjango/urls.py
+++ pidjango/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('pidjango.core.urls')),
+    path('temp/', include('pidjango.twitch.urls')),
     path('accounts/', include('allauth.urls')),
 ]
