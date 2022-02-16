6b33cbd1801b4d197b1d629c256d51d1b9e7eedc
Chris Alvis <alvisleet@gmail.com>
2022-02-16 12:15:31 -0500

    removing old led code

--- pidjango/core/urls.py
+++ pidjango/core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from pidjango.core.views.led_controller import ControlLED
+# from pidjango.core.views.led_controller import ControlLED
 
-urlpatterns = [
-    path('blink-api/', ControlLED.as_view(), name='blink-pattern')
-]
+# urlpatterns = [
+#     path('blink-api/', ControlLED.as_view(), name='blink-pattern')
+# ]
